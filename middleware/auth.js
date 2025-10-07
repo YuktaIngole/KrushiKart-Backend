e59bf6cae2d5d09dc31920d03bab09303507e352
YuktaIngole <142836515+YuktaIngole@users.noreply.github.com>
2025-10-07 10:52:35 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,7 @@ const auth = (req, res, next) => {
     try {
         const token = req.header('x-auth-token');
         if (!token) {
-            return res.status(401).json({ msg: "No authentication token, authorization denied." });
+            return res.status(401).json({ msg: "No authentication token, authorization denie" });
         }
 
         const verified = jwt.verify(token, process.env.JWT_SECRET || "a_secret_key");
